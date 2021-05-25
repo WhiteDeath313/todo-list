e879847f53bf5616f988f757c656b9a9c11b3b62
WhiteDeath313 <arnaud.ridet@epitech.eu>
2021-05-26 00:04:10 +0200

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://https://todo-list-griffith-server.herokuapp.com'
+  apiUrl: 'https://todo-list-griffith-server.herokuapp.com'
 };
