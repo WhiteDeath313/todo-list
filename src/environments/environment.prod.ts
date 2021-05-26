0ba21271b4ded4ea0b01d5d16dbf472965c62962
WhiteDeath313 <arnaud.ridet@epitech.eu>
2021-05-26 02:36:18 +0200

    Zzzz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://todo-list-griffith-server.herokuapp.com',
-  PORT: process.env.PORT
+  apiUrl: 'https://todo-list-griffith-server.herokuapp.com'
 };
