d5950b8c8e40faacf9d7a51d44466416b43fc591
WhiteDeath313 <arnaud.ridet@epitech.eu>
2021-06-05 01:19:44 +0200

    gfds

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use('/', express.static('dist/todo-list'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000, function() {
+    // display which port is used for the user
+    if (process.env.PORT != undefined)
+        console.log("listening on " + process.env.PORT);
+    else
+        console.log("listening on 3000");
+})
 
 //Path Location Strategy
 
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/todo-list/index.html'));
 })
 
+
+console.log(__dirname + '/dist/todo-list/index.html');
 console.log('Console listening!');
